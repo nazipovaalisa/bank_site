a9ceb833e85cf9e7e8d5f92343ba44aedad6b008
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-03-06 17:41:01 +0300

    add index.html

--- bank_project/bankApp/views.py
+++ bank_project/bankApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django import views
 
-# Create your views here.
+class BaseView(views.View): #рендеринг главной страницы
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', {})
