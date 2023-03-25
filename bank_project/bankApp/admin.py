86bc4de19adafadd1762360987e52f7b63bed7e2
alisa.nazipova <alisa.nazipova@sofoil.com>
2023-03-25 11:38:42 +0300

    add admin interface

--- bank_project/bankApp/admin.py
+++ bank_project/bankApp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django import forms
+from .models import *
 
-# Register your models here.
+
+class ServicesInLine(admin.TabularInline):
+
+    model = Transaction.services.through
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+
+    inlines = [ServicesInLine]
+    exclude = ('services', )
+    list_display = ('id', 'date_transaction')
+    list_filter = ('date_transaction', )
+    ordering = ('date_transaction', )
+
+
+admin.site.register(Service)
+
+
+
--- bank_project/bankApp/models.py
+++ bank_project/bankApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+# услуги.
+class Service(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name="Название услуги")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+
+class Transaction(models.Model):
+
+    date_transaction = models.DateField(verbose_name='Дата транзакции')
+    services = models.ManyToManyField(Service, blank=True, related_name='related_transactions', verbose_name='Услуги')
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
+
+    #все услуги в транзакции
+    def services_in_transaction(self):
+        return [serv.name for serv in self.services.all()]
+
